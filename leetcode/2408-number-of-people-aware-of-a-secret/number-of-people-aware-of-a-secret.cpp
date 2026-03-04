e38e9df97f04e315850fd708c07fde7b1cf9b33e
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-03-04 13:37:36 +0530

    Time: 7 ms (28.05%) | Memory: 9.5 MB (53.68%) - LeetSync

---
+++ leetcode/2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<long long> ans(n, 0);
+        const int MOD=1e9+7;
+        long long an = 0;
+        ans[0] = 1;
+        int j = 0;
+        while (j < n) {
+            if (ans[j] > 0) {
+                for (int i = j + delay; i < min(n, j + forget); i++) {
+                    ans[i] = (ans[i] + ans[j])%MOD;
+                }
+            }
+            j++;
+        }
+        for (int i = n - forget; i < n; i++) {
+            an = (an + ans[i]) % MOD;
+        }
+        return an;
+    }
+};
