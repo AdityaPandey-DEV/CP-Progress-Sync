42fa1433dbfbd7dc971798797765380184cc6286
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-04-21 12:00:18 +0530

    Time: 35 ms (33.92%) | Memory: 27.2 MB (47.29%) - LeetSync

---
+++ leetcode/115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    vector<vector<int>> dp;
+    int lcs(int i,int j,string & s1, string &s2){
+        int n1=s1.size(),n2=s2.size();
+        if(j == s2.size()) return 1; 
+        if(i == s1.size()) return 0;  
+        if(dp[i][j] != -1) return dp[i][j];
+        int take=0,notTake=0;
+        if(s1[i]==s2[j]){
+            take=lcs(i+1,j+1,s1,s2);
+        }
+        notTake=lcs(i+1,j,s1,s2);
+        return dp[i][j]=take+notTake;
+
+        
+    }
+public:
+    int numDistinct(string s, string t) {
+        
+        int n=s.size();
+        int m=t.size();
+        dp.assign(n+1,vector<int>(m+1,-1));
+        int ans=lcs(0,0,s,t);
+        return ans;
+    }
+};
