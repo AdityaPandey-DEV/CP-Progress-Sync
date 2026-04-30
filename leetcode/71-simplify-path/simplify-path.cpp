3588585ae04097be2151e623fd556b523936510a
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-04-30 12:12:22 +0530

    Time: 7 ms (19.29%) | Memory: 15.4 MB (10.25%) - LeetSync

---
+++ leetcode/71-simplify-path/simplify-path.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string>st;
+        stringstream ss(path);
+        string token="";
+        while(getline(ss,token,'/')){
+            if(token==""||token==".") continue;
+            if(token!=".."){
+                st.push(token);
+            }
+            else if(!st.empty()){
+                st.pop();
+            }
+        }
+        if(st.empty()){
+            return "/";
+        }
+        string ans="";
+        while(!st.empty()){
+            ans="/"+st.top()+ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
