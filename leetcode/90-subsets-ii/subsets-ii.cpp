d942e1327ff1a1f0ea669e3604edc668cf032583
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-06-29 11:28:21 +0530

    Time: 5 ms (9.35%) | Memory: 17 MB (6.50%) - LeetSync

---
+++ leetcode/90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    vector<vector<int>>ans;
+    void rec(vector<int>s,vector<int>& nums,int i){
+        if(i==nums.size()){
+            ans.push_back(s);
+            return;
+        }
+        int idx=i+1;
+        while(idx<nums.size()&&nums[i]==nums[idx]){
+            idx++;
+        }
+        rec(s,nums,idx);
+        s.push_back(nums[i]);
+        rec(s,nums,i+1);
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(begin(nums),end(nums));
+        vector<int>s={};
+        rec(s,nums,0);
+        return ans;
+    }
+};
