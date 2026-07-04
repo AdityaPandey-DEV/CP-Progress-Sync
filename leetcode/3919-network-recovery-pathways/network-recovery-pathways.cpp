04f02bb92235fc5858f5dba4fa996ace66860c5a
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-07-04 07:30:24 +0530

    Time: 793 ms (20.54%) | Memory: 456.3 MB (15.50%) - LeetSync

---
+++ leetcode/3919-network-recovery-pathways/network-recovery-pathways.cpp
@@ -0,0 +1,65 @@
+class Solution {
+    using ppi = pair<int, int>;
+    using pll = pair<long long,int>;
+    using ll = long long;
+
+    int n;
+    bool check(int mid, ll k,unordered_map<int, vector<vector<int>>> &al) {
+        vector<ll> ans(n,LLONG_MAX);
+        priority_queue<pll,vector<pll>,greater<pll>>pq;
+        ans[0]=0;
+        pq.push({0,0});
+        while(!pq.empty()){
+            ll d=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
+            if(d>k){
+                return false;
+            }
+            if(node==n-1){
+                return true;
+            }
+            if(ans[node]<d)continue;
+            for(auto e:al[node]){
+                int v=e[0];
+                int c=e[1];
+                if(c<mid) continue;
+                if(d+c<ans[v]){
+                    ans[v]=d+c;
+                    pq.push({d+c,v});
+                }
+            }
+        }
+        return false;
+    }
+
+public:
+    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online,
+                         long long k) {
+        n = online.size();
+        unordered_map<int, vector<vector<int>>> al;
+        int l = 1e9, r = 0;
+        for (auto& e : edges) {
+            int u = e[0];
+            int v = e[1];
+            int c = e[2];
+            if (!online[u] || !online[v])
+                continue;
+            al[u].push_back({v, c});
+            r = max(r, c);
+            l = min(l, c);
+        }
+
+        int ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (check(mid, k, al)) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
