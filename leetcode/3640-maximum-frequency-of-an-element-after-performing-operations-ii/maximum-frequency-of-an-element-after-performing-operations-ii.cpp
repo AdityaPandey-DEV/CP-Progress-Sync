948e7ae47db1041a1989e6e29acbc6b4eed3d5ac
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-07-14 15:57:40 +0530

    Time: 1221 ms (49.40%) | Memory: 286.4 MB (46.30%) - LeetSync

---
+++ leetcode/3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        int n=nums.size();
+        int maxe=*max_element(begin(nums),end(nums))+k;
+        map<int,int>diff;
+        unordered_map<int,int>freq;
+        for(int i=0;i<n;i++){
+            int l=max(nums[i]-k,0);
+            int r=min(nums[i]+k,maxe);
+            diff[l]++;
+            diff[r+1]--;
+            diff[nums[i]]+=0;
+            freq[nums[i]]++;
+        }
+        int ans=0;
+        int csum=0;
+        for(auto &[target,sum]:diff){
+            csum+=sum;
+            
+            int targetFreq=freq[target];
+            int need=csum-targetFreq;
+            int maxPossible=min(need,numOperations);
+            ans=max(ans,targetFreq+maxPossible);
+            sum=csum;
+            
+        }
+        return ans;
+    }
+};
