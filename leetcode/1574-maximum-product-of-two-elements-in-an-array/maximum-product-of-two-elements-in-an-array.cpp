65ff9a7cf572d348851296532bb96abb1125cb87
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-07-27 08:35:21 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (25.65%) - LeetSync

--- leetcode/1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ leetcode/1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int l1=1,l2=1;
-        for(int num:nums){
-            if(num>=l1){
-                l2=l1;
-                l1=num;
-                
-                cout<<l2<<" "<<l1<<endl;
+        int l1 = 1, l2 = 1;
+        for (int num : nums) {
+            if (num >= l1) {
+                l2 = l1;
+                l1 = num;
+            } else if (num >= l2) {
+                l2 = num;
             }
-            else
-                if(num>=l2){
-                    l2=num;
-                }
-            
         }
-        cout<<l2<<" "<<l1;
-        return (l2-1)*(l1-1);
+        return (l2 - 1) * (l1 - 1);
     }
 };
