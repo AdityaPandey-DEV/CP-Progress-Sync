b78ee606812dbf57c4032e1997042434a502102f
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-08-01 15:00:02 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (82.54%) - LeetSync

---
+++ leetcode/8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int n = s.size();
+        int i = 0;
+
+        while (i < n && s[i] == ' ')
+            i++;
+
+        bool positive = true;
+
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-')
+                positive = false;
+            i++;
+        }
+
+        long long ans = 0;
+
+        while (i < n && isdigit(s[i])) {
+
+            ans = ans * 10 + (s[i] - '0');
+
+            if (positive && ans > INT_MAX)
+                return INT_MAX;
+
+            if (!positive && -ans < INT_MIN)
+                return INT_MIN;
+
+            i++;
+        }
+
+        return positive ? ans : -ans;
+    }
+};
