2dc8ad06b1b0cf732f2206102a8e266beb1f839b
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-08-02 10:17:47 +0530

    Time: 17 ms (16.30%) | Memory: 19.9 MB (7.37%) - LeetSync

---
+++ leetcode/909-stone-game/stone-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    vector<vector<int>>dp;
+    vector<int>nums;
+    int rec(int i,int j){
+        if(i>j){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int takei=nums[i]-rec(i+1,j);
+        int takej=nums[j]-rec(i,j-1);
+        return dp[i][j]=max(takei,takej);
+    }
+public:
+    bool stoneGame(vector<int>& piles) {
+        nums=piles;
+        int i=0,j=piles.size()-1;
+        dp.assign(j+1,vector<int>(j+1,-1));
+        return rec(i,j)>=0;
+    }
+};
