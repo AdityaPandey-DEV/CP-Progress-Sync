210a56ee4a809c9c23723edcdc106b65c855f7a6
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-08-14 09:24:50 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (96.76%) - LeetSync

---
+++ leetcode/1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        int n = books.size();
+
+        // dp[i] = minimum height required for first i books
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int width = 0;
+            int height = 0;
+
+            // Try placing books j...i on the last shelf
+            for (int j = i; j >= 1; j--) {
+                width += books[j - 1][0];
+
+                // Shelf is full
+                if (width > shelfWidth)
+                    break;
+
+                // Maximum height on the current shelf
+                height = max(height, books[j - 1][1]);
+
+                // Books 1...(j-1) are already optimally placed
+                dp[i] = min(dp[i], dp[j - 1] + height);
+            }
+        }
+
+        return dp[n];
+    }
+};
