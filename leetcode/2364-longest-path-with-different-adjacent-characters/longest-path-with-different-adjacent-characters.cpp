1a4b071160e1432ef59a6c4c4416d654a786a6fd
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-08-15 18:45:01 +0530

    Time: 144 ms (95.29%) | Memory: 162.8 MB (74.08%) - LeetSync

---
+++ leetcode/2364-longest-path-with-different-adjacent-characters/longest-path-with-different-adjacent-characters.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    vector<vector<int>>adj;
+    string s;
+    int ans;
+    int dfs(int u){
+        int ans1=0,ans2=0;
+        for(auto &v:adj[u]){
+            int a=dfs(v);
+            if(s[u]==s[v])continue;
+            if(a>ans1){
+                ans2=ans1;
+                ans1=a;
+            }
+            else if(a>ans2){
+                ans2=a;
+            }
+        }
+        ans=max(ans,1+ans1+ans2);
+        return 1+ans1;
+
+    }
+public:
+    int longestPath(vector<int>& parent, string s) {
+        int n=parent.size();
+        this->s=s;
+        adj.resize(n);
+        for(int i=1;i<n;i++){
+            adj[parent[i]].push_back(i);
+        }
+        ans=0;
+        dfs(0);
+        return ans;
+    }
+};
