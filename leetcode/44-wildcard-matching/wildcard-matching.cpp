289f4a47182a68f55250baba347632b4e50b3f4c
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-08-18 14:01:17 +0530

    Time: 123 ms (14.86%) | Memory: 56.1 MB (6.19%) - LeetSync

---
+++ leetcode/44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    string s, p;
+    int n,m;
+    vector<vector<int>>dp;
+    bool rec(int i,int j){
+        if(i==s.size()&&j==p.size()){
+            return true;
+        }
+        if(i==s.size()+1||j==p.size()+1){
+            return false;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        bool ans=false;
+        if(s[i]==p[j]||p[j]=='?'){
+            ans|=rec(i+1,j+1);
+        }
+        if(p[j]=='*'){
+            ans|=rec(i,j+1)||rec(i+1,j);
+        }
+        return dp[i][j]=ans;
+    }
+public:
+    bool isMatch(string s, string p) {
+        this->s=s;
+        this->p=p;
+        n=s.size();
+        m=p.size();
+        dp.assign(n+2,vector<int>(m+2,-1));
+        return rec(0,0);
+    }
+};
