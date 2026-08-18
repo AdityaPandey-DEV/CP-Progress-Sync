51a37b6cdf32f6f5975031912f740d6485c05450
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-08-18 09:19:15 +0530

    Time: 55 ms (5.26%) | Memory: 30.5 MB (37.22%) - LeetSync

---
+++ leetcode/3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        int n = nums.size();
+        map<int, int> freq;
+        unordered_map<int, int> mp;
+        int i = 0, j = 0;
+        while (j < n) {
+            while (j - i + 1 > k) {
+                mp[nums[i]]--;
+                i++;
+            }
+            mp[nums[j]]++;
+            if (j - i +1 == k) {
+                // cout<<"j:-"<<j<<" i:-"<<i<<endl;
+                for (auto [u, v] : mp) {
+                    if(v>0){
+                    cout<<u<<endl;
+                    freq[u]++;
+                    }
+                }
+            }
+            
+            j++;
+            
+        }
+        int ans = -1;
+        for (auto it = freq.rbegin(); it != freq.rend(); it++) {
+            // cout << it->first << ":" << it->second << endl;
+            if (it->second == 1) {
+                ans = it->first;
+                break;
+            }
+        }
+        return ans;
+    }
+};
