feba3b502a49b007ac5f6442fb7023d67ccaef7c
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-08-23 09:19:24 +0530

    Time: 4 ms (47.55%) | Memory: 13.8 MB (92.07%) - LeetSync

---
+++ leetcode/2039-sum-game/sum-game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int left=0,right=0,leftQ=0,rightQ=0;
+        int n=num.size();
+        for(int i=0;i<n/2;i++){
+            if(num[i]=='?'){
+                leftQ++;
+            }
+            else{
+                left+=num[i]-'0';
+            }
+        }
+        for(int i=n/2;i<n;i++){
+            if(num[i]=='?'){
+                rightQ++;
+            }
+            else{
+                right+=num[i]-'0';
+            }
+        }
+        if((leftQ+rightQ)%2==1)return true;
+        return left-right!=(rightQ-leftQ)*9/2;
+    }
+};
