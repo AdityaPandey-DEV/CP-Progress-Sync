7b580bf2e6da1926cfa9ce4eb7b586bcec14dfdf
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-09-03 09:02:57 +0530

    Time: 0 ms (100.00%) | Memory: 165.9 MB (62.83%) - LeetSync

---
+++ leetcode/4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums) {
+        int mn=*min_element(begin(nums),end(nums));
+        if(mn%2==0){
+            for(auto n:nums){
+                if(n%2!=0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
