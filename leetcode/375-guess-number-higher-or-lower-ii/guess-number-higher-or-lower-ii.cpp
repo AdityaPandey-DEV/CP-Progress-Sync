51570c95f5c844da16f8dc4ee3f2a50220dfad0c
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-09-04 07:53:49 +0530

    Time: 79 ms (38.85%) | Memory: 11 MB (83.10%) - LeetSync

---
+++ leetcode/375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    vector<vector<int>>dp;
+    int rec(int i,int j){
+        if(i>=j){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int ans=1e9;
+        for(int k=i;k<j;k++){
+            int total=k+max(rec(i,k-1),rec(k+1,j));
+            ans=min(ans,total);
+        }
+        return dp[i][j]=ans;
+    }
+public:
+    int getMoneyAmount(int n) {
+        dp.assign(n+1,vector<int>(n+1,-1));
+        return rec(1,n);
+    }
+};
