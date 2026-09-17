e7a16e3830d42c57cab8dd5a73d8da08bba28476
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-09-17 11:09:26 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (83.20%) - LeetSync

---
+++ leetcode/55-jump-game/jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int canreach=0;
+        int i,n=nums.size();
+        if(n==1)return true;
+        for(i=0;i<n-1;i++){
+            if(canreach<i)break;
+            canreach=max(nums[i]+i,canreach);
+            // cout<<i<<endl;
+            
+        } 
+        cout<<i<<endl;
+        return canreach>=n-1;
+        
+    }
+};
