8dabc3062e950a997f79a4c4e65f0cab38437fab
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-09-21 22:13:48 +0530

    Time: 4 ms (22.98%) | Memory: 10.3 MB (89.78%) - LeetSync

---
+++ leetcode/32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int left=0,right=0,maxAns=0;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                left++;
+            }
+            else{
+                right++;
+            }
+            if(left==right){
+                maxAns=max(maxAns,left+right);
+            }
+            else if(right>left){
+                left=0,right=0;
+            }
+        }
+        left=0,right=0;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='('){
+                left++;
+            }
+            else{
+                right++;
+            }
+            if(left==right){
+                maxAns=max(maxAns,left+right);
+            }
+            else if(right<left){
+                left=0,right=0;
+            }
+        }
+        return maxAns;
+    }
+};
